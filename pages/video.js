718312000e28e00e93b02caf9b08234a84db9238
dlopeiro <dribeir4@gmail.com>
2022-11-09 23:35:54 -0300

    dark mode implemented

---
+++ pages/video.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { ColorModeContext } from "../src/components/Menu/components/ColorMode"
+
+
+export default function Video() {
+    const context = React.useContext(ColorModeContext);
+
+    return (
+        <div>
+            Video!
+            {context.mode}
+            <button onClick={() => context.toggleMode()}>
+                Change mode
+            </button>
+        </div>
+    )
+}
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,28 +1,29 @@
 import styled from "styled-components";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search";
 
 const StyledMenu = styled.header`
-display: flex;
-flex-direction: row;
-height: 56px;
-justify-content: space-between;
-background-color: ${({ theme }) => theme.backgroundLevel1 || "#FFFFFF"};
-border: 1px solid ${({ theme }) => theme.borderBase || "#e5e5e5"};
-align-items: center;
-padding: 0 16px;
-gap: 16px;
-position: fixed;
-width: 100%;
-.logo {
+    display: flex;
+    flex-direction: row;
+    height: 56px;
+    justify-content: space-between;
+    background-color: ${({ theme }) => theme.backgroundLevel1 || "#FFFFFF"};
+    border: 1px solid ${({ theme }) => theme.borderBase || "#e5e5e5"};
+    align-items: center;
+    padding: 0 16px;
+    gap: 16px;
+    position: fixed;
     width: 100%;
-    max-width: 80px;
-    @media (min-width: 600px) {
-        max-width: 127px;
+    .logo {
+        width: 100%;
+        max-width: 80px;
+        @media (min-width: 600px) {
+            max-width: 127px;
+        }
+        .text {
+            fill: ${({ theme }) => theme.textColorBase || "#222222"};
+        }
     }
-    .text {
-        fill: ${({ theme }) => theme.textColorBase || "#222222"};
-    }
-}
 `;
 
 export default function Menu({ filterValue, setFilterValue }) {
@@ -32,6 +33,7 @@ export default function Menu({ filterValue, setFilterValue }) {
             <Logo />
         </div>
         <Search filterValue={filterValue} setFilterValue={setFilterValue} />
+        <DarkModeSwitch />
     </StyledMenu>
     );
 }
